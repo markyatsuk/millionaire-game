a6ae97d9ea78712142f3c1f1c1d9a15088346349
Mark <markyatsuk12.04.16@gmail.com>
2022-04-24 00:16:09 +0200

    commit

--- index.js
+++ index.js
@@ -3,12 +3,52 @@ const refs = {
     gameStartCard: document.querySelector('.game-start'),
     gameCard: document.querySelector('.game-section'),
     menuCard: document.querySelector('.menu-section'),
+    menuOpenButton: document.querySelector('.menu-open-btn'),
+    menuCloseButton: document.querySelector('.menu-close-btn'),
+    gameCardDesktop: document.querySelector('.game-desktop'),
+    gameOverCard: document.querySelector('.game-end'),
+    firstAnswerItem: document.querySelector('.to-end-game'),
+    firstAnswerItemDesktop: document.querySelector('.to-end-game-desktop'),
+    endButton: document.querySelector('.end-button'),
+
 }
 
 refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.menuOpenButton.addEventListener('click', toggleMenu);
+refs.menuCloseButton.addEventListener('click', toggleMenu);
+refs.firstAnswerItem.addEventListener('click', endTheGame);
+refs.firstAnswerItemDesktop.addEventListener('click', endTheGameDesktop)
+refs.endButton.addEventListener('click', restartTheGame);
+
 
 function onStartBtnClick(){
-    refs.gameStartCard.classList.add('is-hidden');
+    if (window.innerWidth >= 1440) {
+        refs.gameStartCard.classList.add('is-hidden');
+        refs.gameCardDesktop.classList.remove('is-hidden');
+    }
+    else{
+        refs.gameStartCard.classList.add('is-hidden');
+        refs.gameCard.classList.remove('is-hidden');
+    }
+    
+}
+
+function toggleMenu(){
+    refs.gameCard.classList.toggle('is-hidden');
+    refs.menuCard.classList.toggle('is-hidden');
+}
+
+function endTheGame(){
     refs.gameCard.classList.add('is-hidden');
-    refs.menuCard.classList.remove('is-hidden');
+    refs.gameOverCard.classList.remove('is-hidden');
+}
+
+function endTheGameDesktop(){
+    refs.gameCardDesktop.classList.add('is-hidden');
+    refs.gameOverCard.classList.remove('is-hidden');
+}
+
+function restartTheGame(){
+    refs.gameOverCard.classList.add('is-hidden');
+    refs.gameStartCard.classList.remove('is-hidden');
 }
